8230a95e69cd9a81071be904d3b7f04e7cdcff0a
vickyfrosties <sounia.mhalla@hotmail.com>
2025-02-25 17:17:09 +0100

    Login not working sadge

--- src/services/Login.service.ts
+++ src/services/Login.service.ts
@@ -24,3 +24,32 @@ export async function loginUser(username: string, password: string): Promise<Boo
     throw err;
   }
 }
+
+export async function getUser(): Promise<{ id: number; name: string; } | null> {
+
+  try {
+    const { wpApiSettings } = window as any;
+    if (!wpApiSettings?.root) {
+      console.error("wpApiSettings introuvable");
+      return null;
+    }
+
+    const response = await fetch(`${wpApiSettings.root}wp/v2/users/me`, {
+      credentials: "include",
+      headers: {
+        "X-WP-Nonce": wpApiSettings.nonce
+      }
+    });
+
+    if (!response.ok) {
+      console.error(response.status, await response.text());
+      return null;
+    }
+
+    const data = await response.json();
+    return { id: data.id, name: data.name };
+  } catch (error) {
+    console.error("Erreur lors de la récupération du nonce:", error);
+    return null;
+  }
+};
